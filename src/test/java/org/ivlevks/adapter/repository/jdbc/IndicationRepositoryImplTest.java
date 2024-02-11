fac315d160ec66f168305b21b9bf19d9e2a352d5
ivlevks <ivlevks@yandex.ru>
2024-02-11 15:50:56 +0300

    change mistake after review part 5 tests

--- src/test/java/org/ivlevks/adapter/repository/jdbc/IndicationRepositoryImplTest.java
+++ src/test/java/org/ivlevks/adapter/repository/jdbc/IndicationRepositoryImplTest.java
@@ -1,30 +1,24 @@
 package org.ivlevks.adapter.repository.jdbc;
 
+import org.ivlevks.configuration.DriverManager;
+import org.ivlevks.configuration.migration.MigrationHelper;
 import org.ivlevks.domain.entity.Indication;
 import org.ivlevks.domain.entity.User;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+// такая же история как и с UserRep..Test
 class IndicationRepositoryImplTest {
-    private static final String CREATE_SCHEMA = "CREATE SCHEMA IF NOT EXISTS monitoring";
-    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS monitoring.indications (" +
-            "id serial primary key," +
-            "user_id integer," +
-            "date_time timestamp DEFAULT NOW()," +
-            "heat DOUBLE PRECISION," +
-            "cold_water DOUBLE PRECISION," +
-            "hot_water DOUBLE PRECISION)";
-    private static final String POPULATE_TABLE = "INSERT INTO monitoring.indications (" +
-            "user_id, heat, cold_water, hot_water) VALUES('1', '100', '100', '100')";
+    private static final String DROP_TABLE_USERS = "DROP TABLE monitoring.users";
     private static final String DROP_TABLE_INDICATIONS = "DROP TABLE monitoring.indications";
+    private static final String DROP_TABLE_COUNTERS = "DROP TABLE monitoring.counters";
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             "postgres:15-alpine");
     private Connection connection;
@@ -35,6 +29,9 @@ class IndicationRepositoryImplTest {
     @BeforeAll
     static void beforeAll() {
         postgres.start();
+        org.ivlevks.configuration.DriverManager.setURL(postgres.getJdbcUrl());
+        org.ivlevks.configuration.DriverManager.setUserName(postgres.getUsername());
+        org.ivlevks.configuration.DriverManager.setPASSWORD(postgres.getPassword());
     }
 
     @AfterAll
@@ -44,41 +41,29 @@ class IndicationRepositoryImplTest {
 
     @BeforeEach
     void setUp() {
-        try {
-            connection = DriverManager.getConnection(
-                    postgres.getJdbcUrl(),
-                    postgres.getUsername(),
-                    postgres.getPassword());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        createPopulateUsersTable(connection);
-        indicationRepositoryImpl = new IndicationRepositoryImpl();
-    }
+        connection = DriverManager.getConnection();
 
-    public static void createPopulateUsersTable(Connection connection) {
-        try (PreparedStatement createSchemaStatement = connection.prepareStatement(CREATE_SCHEMA)) {
-            createSchemaStatement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (PreparedStatement createTableStatement = connection.prepareStatement(CREATE_TABLE)) {
-            createTableStatement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (PreparedStatement populateTableStatement = connection.prepareStatement(POPULATE_TABLE)) {
-            populateTableStatement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        MigrationHelper migrationHelper = new MigrationHelper(connection);
+        indicationRepositoryImpl = new IndicationRepositoryImpl();
     }
 
     @AfterEach
     void tearDown() {
-        try (PreparedStatement createTableStatement = connection.prepareStatement(DROP_TABLE_INDICATIONS)) {
-            createTableStatement.executeUpdate();
-            connection.close();
+        Connection connectionDropIndications = org.ivlevks.configuration.DriverManager.getConnection();
+        try (PreparedStatement dropTableIndicationsStatement = connectionDropIndications.prepareStatement(DROP_TABLE_INDICATIONS)) {
+            dropTableIndicationsStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Connection connectionDropUsers = org.ivlevks.configuration.DriverManager.getConnection();
+        try (PreparedStatement dropTableUsersStatement = connectionDropUsers.prepareStatement(DROP_TABLE_USERS)) {
+            dropTableUsersStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Connection connectionDropCounters = org.ivlevks.configuration.DriverManager.getConnection();
+        try (PreparedStatement dropTableCountersStatement = connectionDropCounters.prepareStatement(DROP_TABLE_COUNTERS)) {
+            dropTableCountersStatement.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -87,9 +72,9 @@ class IndicationRepositoryImplTest {
     @Test
     void addIndication() {
         HashMap<String, Double> correctIndications = new HashMap<>();
-        correctIndications.put("heat", 200d);
-        correctIndications.put("cold_water", 200d);
-        correctIndications.put("hot_water", 200d);
+        correctIndications.put("Heat", 200d);
+        correctIndications.put("ColdWater", 200d);
+        correctIndications.put("HotWater", 200d);
         indication = new Indication(correctIndications);
 
         indicationRepositoryImpl.addIndication(user, indication);
